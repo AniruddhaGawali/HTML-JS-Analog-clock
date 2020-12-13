e54ab4f2bfe7153835e6b4eb9d7b61b2f4f73657
AKG007-prog <aniruddhagawali05@gmail.com>
2020-12-13 09:43:15 +0530

    added date

--- js/clock.js
+++ js/clock.js
@@ -1,20 +1,32 @@
 function locaL_time() {
   time = new Date();
-  var time= [time.getHours(),time.getMinutes(),time.getSeconds()];
+  var time = [
+    time.getHours(),
+    time.getMinutes(),
+    time.getSeconds(),
+    time.getDay(),
+    time.getDate(),
+    time.getMonth(),
+  ];
 
-  return time
+  return time;
 }
-
 
 function tz_time(offset) {
   d = new Date();
   utc = d.getTime() + d.getTimezoneOffset() * 60000;
   nd = new Date(utc + 3600000 * offset);
-  var time = [nd.getHours(), nd.getMinutes(), nd.getSeconds()];
+  var time = [
+    nd.getHours(),
+    nd.getMinutes(),
+    nd.getSeconds(),
+    nd.getDay(),
+    nd.getDate(),
+    nd.getMonth(),
+  ];
 
   return time;
 }
-
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -23,6 +35,30 @@ function set_time(time, pos, city) {
   let mrot = 6 * time[1] + time[2] / 10;
   let srot = time[2] * 6;
 
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
   if (city == null) {
     hr.style.transform = `rotate(${hrot}deg)`;
     min.style.transform = `rotate(${mrot}deg)`;
@@ -30,6 +66,8 @@ function set_time(time, pos, city) {
 
     document.getElementById("digital-time").innerHTML =
       time[0] + ":" + time[1] + ":" + time[2];
+    document.getElementById("date").innerHTML =
+      days[time[3]] + "," + " " + time[4] + " " + months[time[5]];
   } else {
     document.getElementsByClassName("tz_hr")[
       pos
@@ -44,16 +82,17 @@ function set_time(time, pos, city) {
     document.getElementsByClassName("tz_name")[pos].innerHTML = city;
     document.getElementsByClassName("tz_digital")[pos].innerHTML =
       time[0] + ":" + time[1] + ":" + time[2];
+    document.getElementsByClassName("tz_date")[pos].innerHTML =
+      days[time[3]] + "," + " " + time[4] + " " + months[time[5]];
   }
 }
-
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------
 
 setInterval(() => {
-  set_time(locaL_time(),null,null);
+  set_time(locaL_time(), null, null);
 
-  set_time(tz_time(0),0,"LONDON");
-  set_time(tz_time(-5),1,"NEW YORK");
-  set_time(tz_time(9),2,"JAPAN");
+  set_time(tz_time(0), 0, "LONDON");
+  set_time(tz_time(-5), 1, "NEW YORK");
+  set_time(tz_time(9), 2, "JAPAN");
 }, 1000);
